6b4ab5fd7d7ccf9d73a34149916278829810b474
Anushka Ekka <anushkaekka23@gmail.com>
2024-08-28 13:58:06 +0530

    Bidirectional

--- app.py
+++ app.py
@@ -14,18 +14,22 @@ def home():
 @app.route('/translate', methods=['POST'])
 def translate_text():
     data = request.json
-    bengali_text = data.get('text')
-    translated = translator.translate(bengali_text, src='bn', dest='hi')
+    text = data.get('text')
+    source_lang = data.get('src_lang')
 
-    # Generate Hindi audio
-    hindi_text = translated.text
-    tts = gTTS(text=hindi_text, lang='hi')
+    # Set destination language based on source language
+    dest_lang = 'hi' if source_lang == 'bn' else 'bn'
+    translated = translator.translate(text, src=source_lang, dest=dest_lang)
+
+    # Generate audio for the translated text
+    translated_text = translated.text
+    tts = gTTS(text=translated_text, lang=dest_lang)
     
     # Use a unique filename to avoid caching issues
     audio_file = f"translated_audio_{int(time.time())}.mp3"
     tts.save(audio_file)
 
-    return jsonify({'translated_text': hindi_text, 'audio_url': f'/{audio_file}'})
+    return jsonify({'translated_text': translated_text, 'audio_url': f'/{audio_file}'})
 
 @app.route('/<audio_file>')
 def get_audio(audio_file):
